e7071f50f185259624dfd6b08256b1ba09b97c73
Nikita Lugovykh <belikovannn@mail.ru>
2021-09-01 12:33:19 +0200

    add feature - aside navigation, havigation from header

--- js/main.js
+++ js/main.js
@@ -1,34 +1,59 @@
-const createSectionNav = (root) => {
-    const nav = root.querySelector(".nav");
-    const main = root.querySelector(".page");
-    let currentActiveStep = nav.querySelector(".active");
+const page = document.querySelector('.page');
+const asideNavbar = document.querySelector(".fidex-navigation");
+const mainNavbar = document.querySelector(".nav");
+
+const createSectionNav = (root, nav) => {
+    let currentActiveStep = nav.querySelector(".nav-active");
   
-    nav.addEventListener("click", ({ target }) => {
-      const { section } = target.closest(".step").dataset;
+    nav.addEventListener("click", ev => {
+      ev.preventDefault()
+      let section 
+      if (nav.dataset.nav === 'aside') {
+        section = ev.target.closest(".step").dataset.section;
+      } 
+      if (nav.dataset.nav === 'main') {
+        section = ev.target.dataset.section;  
+      }
       if (!section) return;
-      main.querySelector(`[data-section='${section}']`).scrollIntoView({
+      console.log(section);
+      root.querySelectorAll(`[data-section='${section}']`)[1].scrollIntoView({
         behavior: "smooth"
       });
     });
-    main.querySelectorAll("section").forEach((s) => {
-      new IntersectionObserver(
-        (entries) => {
-          if (entries[0].isIntersecting) {
-            const { section } = s.dataset;
-            if (!section) return;
-            currentActiveStep.classList.remove("active");
-            const newActiveStep = nav.querySelector(
-              `[data-section='${section}']`
-            );
-            newActiveStep.classList.add("active");
-            currentActiveStep = newActiveStep;
-          }
-        },
-        {
-          threshold: 0.3
-        }
-      ).observe(s);
-    });
-  };
-  createSectionNav(document.querySelector("#page"));
+
+    if (nav.dataset.nav === 'aside') {
+        root.querySelectorAll("section").forEach((item) => {
+        new IntersectionObserver(
+            (entries) => {
+            if (entries[0].isIntersecting) {
+                const { section } = item.dataset;
+                if (!section) return;
+                currentActiveStep.classList.remove("nav-active");
+                const newActiveStep = nav.querySelector(`[data-section='${section}']`);
+                newActiveStep.classList.add("nav-active");
+                currentActiveStep = newActiveStep;
+            }
+            },
+            {
+            threshold: 0.3
+            }
+        ).observe(item);
+        });
+    }
+};
+
+createSectionNav(page, asideNavbar);
+createSectionNav(page, mainNavbar);
+
+const intro = document.querySelector('.intro');
+const nav = document.querySelector('.fidex-navigation');
+function showAsideNavbar() {
+    if(pageYOffset >= intro.offsetHeight) {
+        nav.style.zIndex = 10;
+    } else {
+        nav.style.zIndex = 0;
+    }
+}
+
+document.addEventListener('scroll', showAsideNavbar)
   
