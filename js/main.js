73d9266efbabd951a3aefb461d9ae3d14d15383c
Nikita Lugovykh <belikovannn@mail.ru>
2021-09-01 09:47:40 +0200

    added variables for colors in css, deleted JQuery navigation, begining create navigation by vanila js, added semantics to html

--- js/main.js
+++ js/main.js
@@ -1,81 +1,34 @@
-$(function() {
-
-    var header = $('header'),
-        introH = $('#intro').innerHeight(),
-        scrollOffset = $(window).scrollTop();
-        // При загрузке страницы мы храним в переменной высоту скрола страницы
-        // передаем ее сразу же в переменную и вызываем функцию с таким аргументом
-
-        // Fixed Header
-
-        checkScroll(scrollOffset);
-    // на окно виндов вешается событие скролл
-    $(window).on('scroll', function () {
-        // тут this это window
-        scrollOffset = $(this).scrollTop();
-
-        checkScroll(scrollOffset);
-
+const createSectionNav = (root) => {
+    const nav = root.querySelector(".nav");
+    const main = root.querySelector(".page");
+    let currentActiveStep = nav.querySelector(".active");
+  
+    nav.addEventListener("click", ({ target }) => {
+      const { section } = target.closest(".step").dataset;
+      if (!section) return;
+      main.querySelector(`[data-section='${section}']`).scrollIntoView({
+        behavior: "smooth"
+      });
     });
-
-    function checkScroll(scrollOffset) {
-
-        if (scrollOffset >= introH) {
-        header.addClass('fixed')  
-        } else {
-            header.removeClass('fixed')
+    main.querySelectorAll("section").forEach((s) => {
+      new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            const { section } = s.dataset;
+            if (!section) return;
+            currentActiveStep.classList.remove("active");
+            const newActiveStep = nav.querySelector(
+              `[data-section='${section}']`
+            );
+            newActiveStep.classList.add("active");
+            currentActiveStep = newActiveStep;
+          }
+        },
+        {
+          threshold: 0.3
         }
-   } 
-
-    //  Smoth Scroll  
-   $('[data-scroll]').on('click', function(event) {
-    // отмена стандартного поведения ссылки
-        event.preventDefault();    
-    
-        // Хранится ID элемента 
-        var $this = $(this),
-        blockId = $this.data('scroll'),
-        blockOffset = $(blockId).offset().top;
-        
-        $('#nav a').removeClass('active');
-        $this.addClass('active');
-
-        $('html, body').animate ({
-            scrollTop: blockOffset,
-        }, 1000)
-
+      ).observe(s);
     });
-
-    // Menu nav toggle
-     $('#nav-toggle').on('click', function(event) {
-        event.preventDefault(); 
-        
-        $(this).toggleClass('active');
-        $('#nav').toggleClass('active');
-     })
-
-    //  Collapse
-
-
-    $("[data-collapse]").on('click', function(event){
-        event.preventDefault(); 
-
-        var $this = $(this),
-        blockId = $this.data('collapse');
-
-        $this.toggleClass('active');
-
-        $(blockId).slideToggle();
-    });
-
-
-    // Slider
-
-    $("[data-slider]").slick({
-        infinite: true,
-        fade:true,
-        slidesToShow:1,
-        slidesToScroll:1,
-    });
-
-});
+  };
+  createSectionNav(document.querySelector("#page"));
+  
